72d9d209c25fe1f457587a376cb82189680ae278
Frode Bjerkholt <46349741+FrodeBjerkholt@users.noreply.github.com>
2019-08-28 12:24:16 +0200

    Fixing DPI mock

--- dpimock/src/main/java/com/example/dpimock/DpimockApplication.java
+++ dpimock/src/main/java/com/example/dpimock/DpimockApplication.java
@@ -1,9 +1,12 @@
 package com.example.dpimock;
 
+import model.MessagesSingleton;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(MessagesSingleton.class)
 public class DpimockApplication {
 
     public static void main(String[] args) {
