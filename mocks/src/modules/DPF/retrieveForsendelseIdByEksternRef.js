01a6bd210522d9868f66eadc7a8757c864a3e7e2
mwa <martin.wam@difi.no>
2019-09-11 14:04:15 +0200

    Improved DPF message polling - using username to serve correct messages

--- mocks/src/modules/DPF/retrieveForsendelseIdByEksternRef.js
+++ mocks/src/modules/DPF/retrieveForsendelseIdByEksternRef.js
@@ -1,20 +1,13 @@
 
 retrieveForsendelseIdByEksternRefResponse = function (req, res, parsed) {
-    // 1. Save the externalRef for later:
     let externRef = parsed.envelope.body["0"].retrieveforsendelseidbyeksternref["0"].eksternref["0"];
-
-    let messages = global.dpfDB.get(externRef);
-
-    if (messages) {
-        console.log('break');
-    }
 
     res.send(`<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:ser="http://www.ks.no/svarut/servicesV9">
                    <soap:Header/>
                    <soap:Body>
                       <ser:retrieveForsendelseIdByEksternRefResponse>
                          <!--Zero or more repetitions:-->
-                         ${ messages && `<return>${externRef}<!--Svarut ID. Generert av MOCK.--></return>` }
+                         <return>${externRef}<!--Svarut ID. Generert av MOCK.--></return>
                       </ser:retrieveForsendelseIdByEksternRefResponse>
                    </soap:Body>
                 </soap:Envelope>`);
