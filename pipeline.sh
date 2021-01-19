6108e90e50f7e8c3944377ddaf0fdbf712078db7
Johannes Molland <12180816+johannmo@users.noreply.github.com>
2021-01-19 13:33:07 +0100

    fix: File path.

--- pipeline.sh
+++ pipeline.sh
@@ -9,7 +9,7 @@ sleep 10s
 
 #start sending integrasjonspunkt
 docker pull difimove2/integrasjonspunkt:development
-docker run -d --name sending_integrasjonspunkt --net source_default -p 9093:9093 -e SPRING_PROFILES_ACTIVE=mock -e DIFI_MOVE_SERVICEREGISTRYENDPOINT=http://wiremock:8090 -e DIFI_MOVE_NOARKSYSTEM_ENDPOINTURL=http://sa-mock:8002/p360 -e DIFI_MOVE_DPI_ENDPOINT=http://dpimock:8080/as4 -e DIFI_MOVE_DPV_ENDPOINTURL=http://mocks:8001/dpv/ -e DIFI_MOVE_FIKS_UT_ENDPOINTURL=http://mocks:8001/dpf -e DIFI_MOVE_FIKS_INN_BASEURL=http://mocks:8001/svarinn -e DIFI_MOVE_NEXTMOVE_SERVICEBUS_BASEURL=mocks:8001/dpe -e DIFI_MOVE_DPO_STREAMINGSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasicStreamed.svc?wsdl -e DIFI_MOVE_DPO_BROKERSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasic.svc?wsdl --env-file .\.env difimove2/integrasjonspunkt:development
+docker run -d --name sending_integrasjonspunkt --net source_default -p 9093:9093 -e SPRING_PROFILES_ACTIVE=mock -e DIFI_MOVE_SERVICEREGISTRYENDPOINT=http://wiremock:8090 -e DIFI_MOVE_NOARKSYSTEM_ENDPOINTURL=http://sa-mock:8002/p360 -e DIFI_MOVE_DPI_ENDPOINT=http://dpimock:8080/as4 -e DIFI_MOVE_DPV_ENDPOINTURL=http://mocks:8001/dpv/ -e DIFI_MOVE_FIKS_UT_ENDPOINTURL=http://mocks:8001/dpf -e DIFI_MOVE_FIKS_INN_BASEURL=http://mocks:8001/svarinn -e DIFI_MOVE_NEXTMOVE_SERVICEBUS_BASEURL=mocks:8001/dpe -e DIFI_MOVE_DPO_STREAMINGSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasicStreamed.svc?wsdl -e DIFI_MOVE_DPO_BROKERSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasic.svc?wsdl --env-file ./.env difimove2/integrasjonspunkt:development
               
 
 #Run nextmove tests
