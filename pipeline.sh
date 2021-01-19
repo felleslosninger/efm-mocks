5b10dc2d66a53ee7eb08d7e9264a35baeda0be5e
jaflaten <ja.flaten91@gmail.com>
2021-01-19 13:40:25 +0100

    updated docker run + changing path back to root

--- pipeline.sh
+++ pipeline.sh
@@ -3,13 +3,12 @@
 
 #start mock services
 docker-compose up -d --build --force-recreate
-docker network ls
 sleep 10s
 
 
 #start sending integrasjonspunkt
 docker pull difimove2/integrasjonspunkt:development
-docker run -d --name sending_integrasjonspunkt --net source_default -p 9093:9093 -e SPRING_PROFILES_ACTIVE=mock -e DIFI_MOVE_SERVICEREGISTRYENDPOINT=http://wiremock:8090 -e DIFI_MOVE_NOARKSYSTEM_ENDPOINTURL=http://sa-mock:8002/p360 -e DIFI_MOVE_DPI_ENDPOINT=http://dpimock:8080/as4 -e DIFI_MOVE_DPV_ENDPOINTURL=http://mocks:8001/dpv/ -e DIFI_MOVE_FIKS_UT_ENDPOINTURL=http://mocks:8001/dpf -e DIFI_MOVE_FIKS_INN_BASEURL=http://mocks:8001/svarinn -e DIFI_MOVE_NEXTMOVE_SERVICEBUS_BASEURL=mocks:8001/dpe -e DIFI_MOVE_DPO_STREAMINGSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasicStreamed.svc?wsdl -e DIFI_MOVE_DPO_BROKERSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasic.svc?wsdl --env-file ./.env difimove2/integrasjonspunkt:development
+docker run -d --name sending_integrasjonspunkt --net source_default -p 9093:9093 -e SPRING_PROFILES_ACTIVE=mock -eDIFI_MOVE_SERVICEREGISTRYENDPOINT=http://wiremock:8090 -eDIFI_MOVE_NOARKSYSTEM_ENDPOINTURL=http://sa-mock:8002/p360 -eDIFI_MOVE_DPI_ENDPOINT=http://dpimock:8080/as4 -eDIFI_MOVE_DPV_ENDPOINTURL=http://mocks:8001/dpv/ -eDIFI_MOVE_FIKS_UT_ENDPOINTURL=http://mocks:8001/dpf -eDIFI_MOVE_FIKS_INN_BASEURL=http://mocks:8001/svarinn -eDIFI_MOVE_NEXTMOVE_SERVICEBUS_BASEURL=mocks:8001/dpe -eDIFI_MOVE_DPO_STREAMINGSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasicStreamed.svc?wsdl -eDIFI_MOVE_DPO_BROKERSERVICEURL=http://mocks:8001/dpo/ServiceEngineExternal/BrokerServiceExternalBasic.svc?wsdl difimove2/integrasjonspunkt:development
               
 
 #Run nextmove tests
@@ -19,3 +18,5 @@ cd tests/next-move && npm install && node NextMove.js
 #Remove and shut down
 docker rm sending_integrasjonspunkt --force
 docker-compose down
+
+cd ../..
